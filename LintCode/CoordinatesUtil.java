1b3789ac57682052e2425ced1998ff2ccd781066
Gaaralmn <Gaaralmn@gmail.com>
2015-08-20 18:01:20 -0400

    for updating

--- LintCode/CoordinatesUtil.java
+++ LintCode/CoordinatesUtil.java
@@ -4,7 +4,7 @@ public class CoordinatesUtil {
     class Point implements Comparable<Point> {
         private int x;
         private int y;
-        private distance;
+        private int distance;
 
         public Point(int x, int y) {
             this.x = x;
@@ -14,11 +14,16 @@ public class CoordinatesUtil {
 
         public int getDistanceToZero(int x, int y) {
             int num = x * x + y * y;
-            return Math.sqrt(num);
+            return (int)Math.sqrt(num);
         }
 
         public int compareTo(Point p) {
             return distance - p.distance;
+        }
+
+        public String toString() {
+            String result = x + " , " + y + " and distance is " + distance;
+            return result;
         }
     }
     public Point[] getCoordsNearZero(Point[] points) {
@@ -26,37 +31,41 @@ public class CoordinatesUtil {
             return points;
         }
         Arrays.sort(points);
-        return copyOf(points, 11);
+        return Arrays.copyOf(points, 11);
     }
 
-    public Point[] getPointsFromPath(String path) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(path));
-        } catch (FileNotFoundExeption e) {
-            e.printStackTrace();
-        }
-        List<Point> list = new ArrsyList<Point>();
+    public Point[] getPointsFromPath(String path) throws IOException {
+        List<Point> list = new ArrayList<Point>();
+		File file = new File("engineer.txt");
+        BufferedReader br = new BufferedReader(new FileReader(path));
         String line = br.readLine();
-        while (line.charAt(0) != '8') {
-            line = br.readLine;
+        while (line != null) {
+            line = br.readLine();
+			if (line != null && line.length() > 0 && line.charAt(0) == '8') {
+				break;
+			}
         }
-        while (line != null && line.length() != 0) {
-            String[] temp = line.split("");
+        while (line != null && line.length() > 0) {
+            String[] temp = line.split(", ");
             int x = Integer.parseInt(temp[0]);
             int y = Integer.parseInt(temp[1]);
             list.add(new Point(x, y));
+			line = br.readLine();
         }
         Point[] points = new Point[list.size()];
         for (int i = 0; i < list.size(); i++) {
             points[i] = list.get(i);
         }
+        return points;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CoordinatesUtil CU = new CoordinatesUtil();
-        String path = "./engineer.txt";
+        String path = "engineer.txt";
         Point[] points = CU.getPointsFromPath(path);
-        return CU.getCoordsNearZero(points);
+        Point[] result = CU.getCoordsNearZero(points);
+        for (Point p : result) {
+            System.out.println(p.toString());
+        }
     }
 }
